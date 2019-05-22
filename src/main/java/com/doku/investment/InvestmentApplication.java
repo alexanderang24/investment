e998ef5fc209603f7792429179c5d6c57c287478
laurencebutarbutar <laurencebutarbutar@gmail.com>
2019-05-23 03:18:58 +0700

    Initiate Project

---
+++ src/main/java/com/doku/investment/InvestmentApplication.java
@@ -0,0 +1,34 @@
+package com.doku.investment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * @author Laurence
+ * @see EnableAutoConfiguration, ComponentScan, EnableJpaRepositories, EnableTransactionManagement, EntityScan
+ * <p>
+ * Run Main Investment Application
+ * <p>
+ * Anotation SpringBootApplication is for enable auto-configuration three features anotation 
+ * EnableAutoConfiguration, ComponentScan, Configuration.
+ * <p>
+ * Configure manually : Component, Entities, and Repositories Package will be used, this is forsecure project.
+ * <p>
+ *  
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages={"com.doku.investment"})
+@EnableJpaRepositories(basePackages="com.doku.investment.repositories")
+@EntityScan(basePackages="com.doku.investment.entities")
+public class InvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentApplication.class, args);
+	}
+
+}
